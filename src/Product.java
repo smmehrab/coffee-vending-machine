781a3b5dcd803793faa53f7617a78778009ed9b9
smmehrab <mehrab.24csedu.001@gmail.com>
2022-09-07 06:49:24 +0600

    Product.java initialized

--- src/Product.java
+++ src/Product.java
@@ -1,3 +1,56 @@
 public class Product {
+    private String name;
+    private ProductType type;
+    private int centsPrice;
+    private int cupQuantity;
+    private int secondsPreparationTime;
+
+    public Product(String name, ProductType type, int centsPrice, int cupQuantity, int secondsPreparationTime) {
+        this.name = name;
+        this.type = type;
+        this.centsPrice = centsPrice;
+        this.cupQuantity = cupQuantity;
+        this.secondsPreparationTime = secondsPreparationTime;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ProductType getType() {
+        return type;
+    }
+
+    public void setType(ProductType type) {
+        this.type = type;
+    }
+
+    public int getCentsPrice() {
+        return centsPrice;
+    }
+
+    public void setCentsPrice(int centsPrice) {
+        this.centsPrice = centsPrice;
+    }
+
+    public int getCupQuantity() {
+        return cupQuantity;
+    }
+
+    public void setCupQuantity(int cupQuantity) {
+        this.cupQuantity = cupQuantity;
+    }
+
+    public int getSecondsPreparationTime() {
+        return secondsPreparationTime;
+    }
+
+    public void setSecondsPreparationTime(int secondsPreparationTime) {
+        this.secondsPreparationTime = secondsPreparationTime;
+    }
     
 }
