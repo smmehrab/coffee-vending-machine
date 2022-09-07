f4dbebe91c687b2a00e4f32118ba6092380c0d64
smmehrab <mehrab.24csedu.001@gmail.com>
2022-09-08 05:10:49 +0600

    probably finalized

--- src/App.java
+++ src/App.java
@@ -1,8 +1,5 @@
-import java.nio.file.Paths;
-
 public class App {
     public static void main(String[] args) throws Exception {
-
         Machine machine = Machine.getInstance();
         machine.loadInventory();
         machine.start();
