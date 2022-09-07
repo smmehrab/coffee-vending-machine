f295d4c59800909c818832da6dc0219ea3111c4f
smmehrab <mehrab.24csedu.001@gmail.com>
2022-09-07 07:25:32 +0600

    inventory load successful

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
+import java.nio.file.Paths;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Machine machine = Machine.getInstance();
+        machine.loadInventory();
+
     }
 }
--- src/Machine.java
+++ src/Machine.java
@@ -1,3 +1,9 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Machine {
 
 	/* private instance variable for Singleton Class */
@@ -17,16 +23,24 @@ public class Machine {
     private Money insertedMoney;
 
     /* product variables */
-    private Inventory inventory;
+    private ArrayList<Product> inventory;
     private Product product;
+	private String inventoryFilePath = Paths.get(Paths.get("").toAbsolutePath().toString(), "data", "inventory.txt").toString();
+
+	/* others */
+	private Scanner scanner;
+	private File file;
+
+	/* private constructor */
 
     private Machine() {
-        paymentState = new PaymentState(this);
-		orderState = new OrderState(this);
-		preparationState = new OrderState(this);
-		dispenseState = new DispenseState(this);
-		
+        this.paymentState = new PaymentState(instance);
+		this.orderState = new OrderState(instance);
+		this.preparationState = new OrderState(instance);
+		this.dispenseState = new DispenseState(instance);
     }
+
+	/* public getInstance() */
 
     public static Machine getInstance() {
         if(instance==null) {
@@ -37,5 +51,46 @@ public class Machine {
 
 	public void setState(State state) {
 		this.state = state;
+	}
+
+	public void loadInventory() {
+		file = new File(inventoryFilePath);
+        try {
+            scanner = new Scanner(file);
+        } 
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+		inventory = new ArrayList<>();
+
+		while(scanner.hasNextLine()) {
+			String[] productDetails = scanner.nextLine().split(",");
+			String name = productDetails[0];
+
+			ProductType type = ProductType.Coffee;
+			switch(name) {
+				case "Cappuccino":
+					type = ProductType.Cappuccino;
+					break;
+				default:
+					break;
+			}
+
+			int price = Integer.parseInt(productDetails[1]);
+			int preparationTime = Integer.parseInt(productDetails[2]);
+			int amountLeft = Integer.parseInt(productDetails[3]);
+
+			inventory.add(new Product(name, type, price, preparationTime, amountLeft));
+		}
+
+		// Debug
+		// for(Product product : inventory) {
+		// 	System.out.println(product.getName());
+		// 	System.out.println(product.getType().toString());
+		// 	System.out.println(product.getPrice());
+		// 	System.out.println(product.getPreparationTime());
+		// 	System.out.println(product.getAmountLeft());
+		// }
 	}
 }
