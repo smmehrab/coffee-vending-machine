104acc89f07c5b3daeae2eb3c620d98eef9c81f8
smmehrab <mehrab.24csedu.001@gmail.com>
2022-09-07 06:50:42 +0600

    Machine.java initialized

--- src/Machine.java
+++ src/Machine.java
@@ -1,7 +1,32 @@
 public class Machine {
+
+	/* private instance variable for Singleton Class */
     private static Machine instance;
 
-    private Machine() {}
+    /* current state */
+    private State state;
+
+    /* possible states */
+    private State paymentState;
+    private State orderState;
+    private State preparationState;
+    private State dispenseState;
+
+    /* money variables */
+    private Money machineMoney;
+    private Money insertedMoney;
+
+    /* product variables */
+    private Inventory inventory;
+    private Product product;
+
+    private Machine() {
+        paymentState = new PaymentState(this);
+		orderState = new OrderState(this);
+		preparationState = new OrderState(this);
+		dispenseState = new DispenseState(this);
+		
+    }
 
     public static Machine getInstance() {
         if(instance==null) {
@@ -10,5 +35,7 @@ public class Machine {
         return instance;
     }
 
-    
+	public void setState(State state) {
+		this.state = state;
+	}
 }
