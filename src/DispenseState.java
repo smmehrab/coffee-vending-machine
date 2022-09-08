c0dc84c8ef8e3282b98a383fa885e53f2889462c
smmehrab <mehrab.24csedu.001@gmail.com>
2022-09-08 22:25:14 +0600

    DispenseState.java implemented

--- src/DispenseState.java
+++ src/DispenseState.java
@@ -1,34 +1,133 @@
+import java.io.Console;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.LogManager;
+
 public class DispenseState extends State {
 
     private Machine machine;
 
     public DispenseState(Machine machine) {
         this.machine = machine;
+
+        System.out.println();
+		System.out.print(ConsoleColor.GREEN_BOLD);
+		System.out.println("Dispense");
+		System.out.println("-----------------------");
+		System.out.print(ConsoleColor.RESET);
+		System.out.println();
+        
+        // Debug
+        // System.out.println(this.machine.getReturnMoney().getStatus());
+        // System.out.println(this.machine.getReturnMoney().getMoney().getAmount());
+
         this.dispenseMoney();
     }
     
     @Override
     public void insertCoin() {
         /* Go to PaymentState */
-        
+        this.machine.clearPaidMoney();
+        this.machine.setState(new PaymentState(machine));
     }
 
     @Override
     public void pressButton() {
-        // TODO Auto-generated method stub
+        /* Invalid for DispenseState */
         
     }
 
     @Override
     public void dispenseProduct() {
-        // TODO Auto-generated method stub
-        
+        System.out.print(ConsoleColor.GREEN_BRIGHT);
+        System.out.println("[Coffee Ready]");
+        System.out.print(ConsoleColor.RESET);
+        System.out.println();
+
+        System.out.print("Press ENTER to take the coffee...");
+        char[] temp = System.console().readPassword();
+
+        System.out.println("");
+        System.out.print(ConsoleColor.GREEN_BRIGHT);
+        System.out.println("[Enjoy Your Coffee]");
+        System.out.print(ConsoleColor.RESET);
+        System.out.println("\n");
+
+        this.insertCoin();
     }
 
     @Override
     public void dispenseMoney() {
-        System.out.println(this.machine.getReturnMoney().getStatus());
-        System.out.println(this.machine.getReturnMoney().getMoney().getAmount());
+        Money returnMoney = this.machine.getReturnMoney().getMoney();
+
+        System.out.print("Dispensed: ");
+        System.out.print(ConsoleColor.GREEN_BRIGHT);
+        System.out.print(Integer.toString(returnMoney.getAmount()) + " Cents");
+        System.out.print(ConsoleColor.RESET);
+		System.out.println("\n");
+
+		String stringFormat = "%10s: %d";
+        ArrayList<String> moneyTypes = Money.getTypes();
+        for(String type : moneyTypes) {
+            System.out.printf(stringFormat, type, returnMoney.getCount(type));
+            System.out.println();
+		}
+		System.out.println();
+
+        this.machine.clearReturnMoney();
+        this.prepareCoffee();
+    }
+
+    private void prepareCoffee() {
+        System.out.println("Preparing Your Coffee ...");
+        System.out.println();
+
+        System.out.print(ConsoleColor.GREEN_BRIGHT);
+
+        int preparationTime = 10;
+        for (int i = 0; i <= preparationTime; i += 1) {
+            progressPercentage(i, preparationTime);
+            try {
+                Thread.sleep(1000);
+            } 
+            catch (Exception e) {
+                
+            }
+        }
+        
+        System.out.print(ConsoleColor.RESET);
+        System.out.println();
+
+        this.dispenseProduct();
     }
     
+    private void progressPercentage(int done, int total) {
+        int size = 21;
+        String iconLeftBoundary = "[";
+        String iconDone = "=";
+        String iconRemain = ".";
+        String iconRightBoundary = "]";
+
+        if (done > total) {
+            throw new IllegalArgumentException();
+        }
+        int donePercents = (100 * done) / total;
+        int doneLength = size * donePercents / 100;
+
+        StringBuilder bar = new StringBuilder(iconLeftBoundary);
+        for (int i = 0; i < size; i++) {
+            if (i < doneLength) {
+                bar.append(iconDone);
+            } else {
+                bar.append(iconRemain);
+            }
+        }
+        bar.append(iconRightBoundary);
+
+        System.out.print("\r" + bar + " " + donePercents + "%");
+
+        if (done == total) {
+            System.out.print("\n");
+        }
+    }
 }
